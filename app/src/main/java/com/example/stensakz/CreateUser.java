3c88cabec68a6bcc1560a0cf189b8f587404cc67
heimgun <heidi.guneriussen@live.no>
2020-03-12 17:36:27 +0100

    Edittext++

--- app/src/main/java/com/example/stensakz/CreateUser.java
+++ app/src/main/java/com/example/stensakz/CreateUser.java
@@ -2,6 +2,10 @@ package com.example.stensakz;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,10 +26,29 @@ public class CreateUser extends AppCompatActivity {
 
 
         //XML-objects
+        Button CUBtn = (Button) findViewById(R.id.CUBtn);
+        final EditText CUusername = (EditText) findViewById(R.id.CUEditUsername);
+        final EditText CUpassword = (EditText) findViewById(R.id.CUEditpassword);
+        final EditText CUemail = (EditText) findViewById(R.id.CUEditEmail);
+        final TextView usernameEmpty = (TextView) findViewById(R.id.UsernameEmpty);
+        usernameEmpty.setVisibility(View.INVISIBLE);
 
 
         //Methods
+        CUBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CUusername.getText().toString() != null && CUpassword.getText().toString() != null && CUemail.getText().toString() != null){
 
+                }
+
+                else if (CUusername.getText().toString() == null){
+
+                    usernameEmpty.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
 
     }
 }
