9f321b9f32bd5dfaef44eefb99677afd356723aa
heimgun <heidi.guneriussen@live.no>
2020-03-18 17:03:26 +0100

    Getting there

--- app/src/main/java/com/example/stensakz/StartGame.java
+++ app/src/main/java/com/example/stensakz/StartGame.java
@@ -1,8 +1,10 @@
 package com.example.stensakz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +14,11 @@ import java.util.Random;
 public class StartGame extends AppCompatActivity {
 
 
-
-    private final int rock = 1;
-    private final int paper = 2;
-    private final int scissor = 3;
     Random generator = new Random();
-    Button rockBtn, paperBtn, scissorBtn;
+    Button rockBtn, paperBtn, scissorBtn, reStartBtn;
     RPS computerPlay, playerPlay;
+    TextView gameTV;
+
 
 
 
@@ -29,14 +29,15 @@ public class StartGame extends AppCompatActivity {
         setContentView(R.layout.startgame);
 
         /* TODO
-            1. RPS COMPUTER RANDOM
-            2. BEATS
-            3. GUI
+            1. BEATS
+            2. GUI
          */
 
         rockBtn = (Button) findViewById(R.id.Rock);
         paperBtn = (Button) findViewById(R.id.Paper);
         scissorBtn = (Button) findViewById(R.id.Scissor);
+        reStartBtn = (Button) findViewById(R.id.Restart);
+        gameTV = (TextView) findViewById(R.id.GameTV);
 
 
         rockBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,9 +45,28 @@ public class StartGame extends AppCompatActivity {
             public void onClick(View v) {
 
                 playerPlay = RPS.ROCK;
+                cpGenerator();
+                gameTV.getText().toString();
+
+                if (playerPlay.beats(computerPlay)){
+
+                    gameTV.setText("You win");
 
 
+                }
 
+                else if (computerPlay.beats(playerPlay)){
+
+                    gameTV.setText("Computer win");
+                }
+
+                else if (computerPlay.equals(playerPlay)){
+
+                    gameTV.setText("TIE");
+
+                }
+
+                return;
 
             }
         });
@@ -56,8 +76,28 @@ public class StartGame extends AppCompatActivity {
             public void onClick(View v) {
 
                 playerPlay = RPS.PAPER;
+                cpGenerator();
+                gameTV.getText().toString();
+
+                if (playerPlay.beats(computerPlay)){
+
+                    gameTV.setText("You win");
 
 
+                }
+
+                else if (computerPlay.beats(playerPlay)){
+
+                    gameTV.setText("Computer win");
+                }
+
+                else if (computerPlay.equals(playerPlay)){
+
+                    gameTV.setText("TIE");
+
+                }
+
+                return;
 
             }
         });
@@ -67,14 +107,88 @@ public class StartGame extends AppCompatActivity {
             public void onClick(View v) {
 
                 playerPlay = RPS.SCISSORS;
+                cpGenerator();
+
+                gameTV.getText().toString();
+
+                if (playerPlay.beats(computerPlay)){
+
+                    gameTV.setText("You win");
 
 
+                }
+
+                else if (computerPlay.beats(playerPlay)){
+
+                    gameTV.setText("Computer win");
+                }
+
+                else if (computerPlay.equals(playerPlay)){
+
+                    gameTV.setText("TIE");
+
+                }
+
+                return;
 
             }
         });
 
 
 
+        reStartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartGame.this, StartGame.class));
+            }
+        });
+
+
+    }
+
+
+    public void cpGenerator(){
+
+        int cp = generator.nextInt(3)+1;
+
+        if (cp == 1){
+            computerPlay = RPS.ROCK;
+        }
+
+        else if (cp == 2){
+            computerPlay = RPS.PAPER;
+        }
+
+        else if (cp == 3){
+            computerPlay = RPS.SCISSORS;
+        }
+
+
+
+    }
+
+
+
+    public enum RPS {
+
+        ROCK, PAPER, SCISSORS;
+
+        boolean beats(RPS other){
+
+            switch(this) {
+                case ROCK: return other == SCISSORS;
+                case PAPER: return other == ROCK;
+                case SCISSORS: return other == PAPER;
+
+                default:
+                    throw new IllegalStateException();
+
+            }
+
+
+        }
+
+    }
 
 
 
@@ -83,33 +197,10 @@ public class StartGame extends AppCompatActivity {
 
 
 
-    public enum RPS{
-
-       ROCK, PAPER, SCISSORS;
-
-       private static boolean beats(RPS other) {
-           return true;
-       }
-
-       static {
-
-           ROCK.beats(SCISSORS);
-           PAPER.beats(ROCK);
-           SCISSORS.beats(PAPER);
-
-       }
-
-
-
-    }
 
 
 
 
-
-
-
-    }
 
 
 
